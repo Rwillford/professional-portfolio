7feea4b816e1ea9e35ef1ec87e8f1e13c304a49a
Bob <rwillford0719@gmail.com>
2022-07-20 21:08:34 -0500

    added component folders

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../Navigation';
+
+function Header() {
+    return (
+        <header>
+            <h1>Robert Willford</h1>
+            <Nav></Nav>
+        </header>
+    )
+}
