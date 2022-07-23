a3303a788310bb471411cb00a26203e7df1e7b13
Bob <rwillford0719@gmail.com>
2022-07-23 14:56:33 -0500

    pushing before tryting big change

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import recipe from "../../assets/images/RecipeRoulette.jpg"
 
 function Portfolio() {
+    const currentProject = {
+        title: 'Recipe Roulette',
+        description: 'An App to randomly choose a food & drink recipe',
+        github: 'https://github.com/rehpotsirhc21/recipe-finder',
+        deploy: 'https://rehpotsirhc21.github.io/recipe-finder/'
+    }
     return (
         <section>
-            <h3>title</h3>
-            <p>description</p>
-            {/*<img>image</img>*/}
-            <p><a href="link">GitHub</a></p>
-            <p><a href="link">Deploy</a></p>
+            <h3>{currentProject.title}</h3>
+            <p>{currentProject.description}</p>
+            <div>
+                <img 
+                    src={recipe} 
+                    style={{width: "400px"}}  
+                    alt="test pic"
+                    className="img-thumbnail mx-1"
+                    />
+            </div>
+                <p><a href={currentProject.github}>GitHub Repository</a></p>
+                <p><a href={currentProject.deploy}>Deployed Site</a></p>
         </section>
     )
 }
